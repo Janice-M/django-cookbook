5260b8372c224d7973a9a0314d4dca89e9064fb7
Janice-M <janiceink001@gmail.com>
2020-08-09 23:17:36 +0300

    class ingredient

--- cookbook/ingredients/models.py
+++ cookbook/ingredients/models.py
@@ -6,3 +6,13 @@ class Category(models.Model):
 
     def __str__ (self):
         return self.name
+    
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    notes = models.TextField()
+    category = models.ForeignKey(
+        Category, related_name="ingredients", on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
