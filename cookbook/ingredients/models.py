143fec999c43d287b1908288e850cae64cabf258
Janice-M <janiceink001@gmail.com>
2020-08-11 23:06:40 +0300

    class ingredient migration

--- cookbook/ingredients/models.py
+++ cookbook/ingredients/models.py
@@ -5,7 +5,7 @@ class Category(models.Model):
     name= models.CharField(max_length=100)
 
     def __str__ (self):
-        return self.name
+        return self.
     
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
