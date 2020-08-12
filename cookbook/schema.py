aeb2e108db47d5490bbda366c04042869e712bb3
Janice-M <janiceink001@gmail.com>
2020-08-12 22:58:04 +0300

    class Query

--- cookbook/schema.py
+++ cookbook/schema.py
@@ -14,3 +14,6 @@ class IngredientType(DjangoObjectType):
         model= Ingredient
         fields= ("id", "name", "notes", "category")
         
+class Query (graphene.ObjectType):
+    all_ingredients= graphene.List(IngredientType)
+    category_by_name = graphene.Field(CategoryType, name=graphene.String(required=True))
