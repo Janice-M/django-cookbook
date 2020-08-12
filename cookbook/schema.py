c2a1f9293fb332ae35159b2d13ab1646be27d900
Janice-M <janiceink001@gmail.com>
2020-08-12 23:06:43 +0300

    optimizing query count

--- cookbook/schema.py
+++ cookbook/schema.py
@@ -22,3 +22,9 @@ class Query (graphene.ObjectType):
         #optimizing query count using resolve method
         
         return Ingredient.objects.select_related("category").all()
+        
+    def resolve_category_by_name (root, info, name):
+        try:
+            return Category.objects.get(name=name)
+        except Category.DoesNotExist:
+            return None
