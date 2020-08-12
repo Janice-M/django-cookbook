f96035f7f70ad59ea0b774950c0c8ec2871cc9a7
Janice-M <janiceink001@gmail.com>
2020-08-12 22:50:24 +0300

    category type

--- cookbook/schema.py
+++ cookbook/schema.py
@@ -2,3 +2,9 @@
 
 import graphene 
 from graphene_django import DjangoObjectType
+from cookbook.ingredients.models import Category, Ingredient
+
+class CateforyType(DjangoObjectType):
+    class Meta:
+        model=Category
+        fields = ("id", "name" ,"ingredients")
