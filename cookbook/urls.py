85d799283d72e172af2702ea07244974bdc2b2d5
Janice-M <janiceink001@gmail.com>
2020-08-17 10:54:43 +0300

    specifying schema

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -21,5 +21,5 @@ from cookbook.schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
 ]
