f47c64ca5419d11c48ee21ff86429777391e578f
Janice-M <janiceink001@gmail.com>
2020-08-17 10:53:55 +0300

    specifying schema

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
+from cookbook.schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
