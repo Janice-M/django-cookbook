69bba96232a8b70f00426db56cd268c2d76fab06
Janice-M <janiceink001@gmail.com>
2020-08-18 23:26:20 +0300

    specifying schema

--- cookbook/ingredients/models.py
+++ cookbook/ingredients/models.py
@@ -7,7 +7,7 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-class Ingredient(models.Model):
+class Ingredient(models.Model)
     name = models.CharField(max_length=100)
     notes = models.TextField()
     category = models.ForeignKey(
