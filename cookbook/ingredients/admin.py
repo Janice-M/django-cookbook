539941d17efb29f491d96013e495bd8e0c73bebf
Janice-M <janiceink001@gmail.com>
2020-08-20 23:42:58 +0300

    admin

--- cookbook/ingredients/admin.py
+++ cookbook/ingredients/admin.py
@@ -4,4 +4,4 @@ from cookbook.ingredients.models import Category, Ingredient
 # Register your models here.
 
 admin.site.register(Category)
-admin.site.register(Ingredient
+admin.site.register(Ingredient)
